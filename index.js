c8aedd015eab18c08f7ea19451c591cd6c3f1a4c
Chase Wasden <cwasden@anonyome.com>
2018-07-29 17:59:09 -0600

    added pinterest

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ import * as _google from './lib/providers/google';
 import * as _facebook from './lib/providers/facebook';
 import * as _twitter from './lib/providers/twitter';
 import * as _tumblr from './lib/providers/tumblr';
+import * as _pinterest from './lib/providers/pinterest';
 
 export const google = login(_google, platform);
 export const facebook = login(_facebook, platform);
 export const twitter = login(_twitter, platform);
 export const tumblr = login(_tumblr, platform);
+export const pinterest = login(_pinterest, platform);
 
 export default login(__, platform);
